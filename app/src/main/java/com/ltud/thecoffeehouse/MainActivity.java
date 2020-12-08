f449b7cda26d83070ddd2f9abc6ee5fb7cc6c9f5
Quyen <Quyen@Nhats-iMac.local>
2020-12-08 21:36:36 +0700

    Fix fragment double click problem

--- app/src/main/java/com/ltud/thecoffeehouse/MainActivity.java
+++ app/src/main/java/com/ltud/thecoffeehouse/MainActivity.java
@@ -7,38 +7,45 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationBar;
+    FrameLayout mainFrame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setDefaultFragment();
         bottomNavigationBar = findViewById(R.id.bottom_nav_bar);
-        bottomNavigationBar.setOnNavigationItemReselectedListener(navListener);
-    }
-    private BottomNavigationView.OnNavigationItemReselectedListener navListener =
-            new BottomNavigationView.OnNavigationItemReselectedListener() {
-                @Override
-                public void onNavigationItemReselected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
-                    switch (item.getItemId()){
-                        case R.id.news:
-                            selectedFragment = new NewsFragment();
-                            break;
-                        case R.id.order:
-                            selectedFragment = new OrderFragment();
-                            break;
-                        case R.id.store:
-                            selectedFragment = new StoreFragment();
-                            break;
-                        case R.id.user:
-                            selectedFragment = new UserFragment();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.bottom_wrapper,selectedFragment).commit();
+        mainFrame = findViewById(R.id.bottom_wrapper);
+        bottomNavigationBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()){
+                    case R.id.news:
+                        selectedFragment = new NewsFragment();
+                        break;
+                    case R.id.order:
+                        selectedFragment = new OrderFragment();
+                        break;
+                    case R.id.store:
+                        selectedFragment = new StoreFragment();
+                        break;
+                    case R.id.user:
+                        selectedFragment = new UserFragment();
+                        break;
                 }
-            };
+                getSupportFragmentManager().beginTransaction().replace(R.id.bottom_wrapper,selectedFragment).commit();
+                return true;
+            }
+        });
+    }
+    private void setDefaultFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.bottom_wrapper, new NewsFragment()).commit();
+    }
+
 }
