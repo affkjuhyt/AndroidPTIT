ea5be89c29fec8b4828f530cc26831f8f9eae40c
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-12-11 10:25:38 +0700

    Set time splash

--- app/src/main/java/com/ltud/thecoffeehouse/SplashActivity.java
+++ app/src/main/java/com/ltud/thecoffeehouse/SplashActivity.java
@@ -2,6 +2,7 @@ package com.ltud.thecoffeehouse;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class SplashActivity extends AppCompatActivity {
@@ -9,8 +10,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Start home activity
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        // close splash activity
-        finish();
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity .class));
+
+                // close splash activity
+                finish();
+            }
+        }, secondsDelayed * 1000);
     }
 }
