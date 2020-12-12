c1c5aa3c4b3c0d20d6cf8ae51add1c9708001b3e
nguyenkimdao1998 <nguyenkimdao1998@gmail.com>
2020-12-12 09:20:14 +0700

    Pull request

--- app/src/main/java/com/ltud/thecoffeehouse/OrderFragment.java
+++ app/src/main/java/com/ltud/thecoffeehouse/OrderFragment.java
@@ -7,14 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OrderFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class OrderFragment extends Fragment {
-
+    RadioGroup radioGroup;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
